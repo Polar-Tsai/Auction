b6cd7f78e2e5b65cf4eb5c0b9bfbfcccb6a6cd63
Polar-Tsai <polartsai@gmail.com>
2025-12-24 10:07:10 +0800

    Fix media file routing - serve media files without language prefix

--- auction_site/urls.py
+++ auction_site/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     # path('admin/', admin.site.urls), # Disabled custom admin
@@ -8,8 +10,6 @@ urlpatterns = i18n_patterns(
     prefix_default_language=True,  # Include language prefix even for default language
 )
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Media files should be served without language prefix
+# In production, use a web server (nginx/apache) to serve these files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
