7439a30bb81c62b2fdc83fcf8e2be7ce3801523f
Polar-Tsai <polartsai@gmail.com>
2025-12-24 10:34:53 +0800

    Fix media file serving with custom view for data_photo

--- auction_site/urls.py
+++ auction_site/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
-urlpatterns = i18n_patterns(
+# Media files served without language prefix
+urlpatterns = [
+    re_path(r'^data_photo/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Language-specific URLs
+urlpatterns += i18n_patterns(
     # path('admin/', admin.site.urls), # Disabled custom admin
     path('', include('auctions.urls')),
     prefix_default_language=True,  # Include language prefix even for default language
 )
-
-# Media files should be served without language prefix
-# In production, use a web server (nginx/apache) to serve these files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
